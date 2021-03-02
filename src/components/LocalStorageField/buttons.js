6c93d0b558a1e1e6c61d46fd354d7536fc885877
Spenser Schwartz <spensernet@gmail.com>
2021-03-02 15:58:19 -0800

    Add button file for all buttons to be imported from

---
+++ src/components/LocalStorageField/buttons.js
@@ -0,0 +1,5 @@
+import FetchLocalStorageButton from './FetchLocalStorageButton';
+import ClearButton from './ClearButton';
+import SaveButton from './SaveButton';
+
+export {FetchLocalStorageButton, ClearButton, SaveButton}
