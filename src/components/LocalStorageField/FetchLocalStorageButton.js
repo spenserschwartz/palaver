07a79622e899595c230b5f9df382ccfb02ba17aa
Spenser Schwartz <spensernet@gmail.com>
2021-03-02 15:56:20 -0800

    Add FetchLocalStorageButton as its own component

---
+++ src/components/LocalStorageField/FetchLocalStorageButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {fetchLocalStorage} from '../../helpers/functions';
+
+const FetchLocalStorageButton = () => {
+
+const buttonClick = () => {
+    console.log('Fetchy');
+
+    const localObject = JSON.parse(fetchLocalStorage());
+    console.log('LO: ', localObject)
+}
+
+  return (
+    <div>
+      <button onClick={() => buttonClick()}>Fetch Local Storage</button>
+    </div>
+  )
+}
+
+export default FetchLocalStorageButton
