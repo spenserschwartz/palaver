96e0e5f612b0de7e239c5b13d9f74f1d0897e4a0
Spenser Schwartz <spensernet@gmail.com>
2021-03-02 15:54:19 -0800

    Cleanup and refactoring

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React, {useState} from 'react';
 import './App.css';
 
 import {LocalStorageField, InputField, ResultsField} from './components'
-
-//import fetchLocalStorage from './Data';   // Fetch local storage
-import {inputObjectTemplate, introMessageTemplate1} from './helpers'
+import {inputObjectTemplate} from './helpers'
 
 
 function App() {
-  //const [palaverObject, setPalaverObject] = useState(fetchLocalStorage());
-
-
   const [inputObject, setInputObject] = useState(inputObjectTemplate);
 
   return (
