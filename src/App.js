eccefdc07a92081d95b413856d5da0b90c95ebe0
Spenser Schwartz <spensernet@gmail.com>
2021-03-04 07:24:06 -0800

    Prettier and reformat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, {useState} from 'react';
-import './App.css';
-
-import {LocalStorageField, InputField, ResultsField} from './components'
-import {inputObjectTemplate} from './helpers'
-
+import React, { useState } from 'react';
+import { LocalStorageField, InputField, ResultsField } from './components';
+import { inputObjectTemplate } from './helpers';
 
 function App() {
   const [inputObject, setInputObject] = useState(inputObjectTemplate);
 
   return (
     <div className="App">
-      <LocalStorageField inputObject={inputObject} setInputObject={setInputObject}
+      <LocalStorageField
+        inputObject={inputObject}
+        setInputObject={setInputObject}
       />
 
       <InputField inputObject={inputObject} setInputObject={setInputObject} />
 
       <ResultsField inputObject={inputObject} setInputObject={setInputObject} />
-      
     </div>
   );
 }
