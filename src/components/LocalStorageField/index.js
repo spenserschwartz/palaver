36cc1278a63ecac7ef6ad294aa96dbf959e5824e
Spenser Schwartz <spensernet@gmail.com>
2021-03-04 07:22:04 -0800

    Prettier format

--- src/components/LocalStorageField/index.js
+++ src/components/LocalStorageField/index.js
@@ -1,26 +1,23 @@
-import '../styles.css'
-import {ClearButton, SaveButton, FetchLocalStorageButton} from './buttons';
+import '../styles.css';
+import { ClearButton, SaveButton, FetchLocalStorageButton } from './buttons';
 import LocalStorageDisplay from './LocalStorageDisplay';
-import {fetchLocalStorage} from '../../helpers/functions'
+import { fetchLocalStorage } from '../../helpers/functions';
 
-const LocalStorageField = ({inputObject, setInputObject}) => {
+const LocalStorageField = ({ inputObject, setInputObject }) => {
   const localObject = JSON.parse(fetchLocalStorage());
   // const object = {coffeeZ: 'cup', chicken: 'eggsf', number: 33}
   // localStorage.setItem('Palaver', JSON.stringify(object));\
 
-
   return (
     <div className="buttonField">
       <h1>Local Storage Field</h1>
-      <SaveButton 
-      />
+      <SaveButton />
       <ClearButton />
-      
-      <FetchLocalStorageButton />
-      <LocalStorageDisplay  localObject={localObject} />
-    </div>
-  )
 
-}
+      <FetchLocalStorageButton />
+      <LocalStorageDisplay localObject={localObject} />
+    </div>
+  );
+};
 
 export default LocalStorageField;
