503f339f0ad7b8e44c7a397526d19395073b3a5b
Spenser Schwartz <spensernet@gmail.com>
2021-03-05 15:05:45 -0800

    Personal In textbox classname designated to be different

--- src/components/InputField/InputRow.js
+++ src/components/InputField/InputRow.js
@@ -1,25 +1,26 @@
-import React, {useState} from 'react'
-import {displayName} from '../../helpers/functions'
-import './styles.css'
+import React, { useState } from 'react';
+import { displayName } from '../../helpers/functions';
+import './styles.css';
 
 const InputRow = ({ row, index }) => {
   const [input, setInput] = useState('');
 
-  const inputProps = {
-  }
+  const inputProps = {};
 
   inputProps.required = row === 'personalIn' ? false : true;
 
   return (
     <div className="inputRow">
       <div className="inputLabel">{displayName(row)}</div>
-      <textarea className="smallInput"
-        value={input} onChange={e => setInput(e.target.value)}
+      <textarea
+        className={row === 'personalIn' ? 'largeinput' : 'smallInput'}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         id={row}
         {...inputProps}
       />
     </div>
-  )
-}
+  );
+};
 
-export default InputRow
+export default InputRow;
