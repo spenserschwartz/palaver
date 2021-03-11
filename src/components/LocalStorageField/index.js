88bbc06b1d45ecf67cfb10b6ec47d07126d440bc
Spenser Schwartz <spensernet@gmail.com>
2021-03-11 10:11:41 -0800

    Change localObject to React Hook, add initial useEffect

--- src/components/LocalStorageField/index.js
+++ src/components/LocalStorageField/index.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import '../styles.css';
 import { ClearButton, SaveButton, FetchLocalStorageButton } from './buttons';
 import LocalStorageDisplay from './LocalStorageDisplay';
@@ -5,15 +6,27 @@ import { inputObjectTemplate } from '../../helpers';
 import { fetchLocalStorage } from '../../helpers/functions';
 
 const LocalStorageField = ({ inputObject, setInputObject }) => {
-  let localObject = JSON.parse(fetchLocalStorage());
+  //let localObject = JSON.parse(fetchLocalStorage());
 
   // Check to match template. If Palaver key in localStorage does not have the template keys, localObject is reset to the template
-  for (let key in inputObjectTemplate) {
-    if (!localObject[key]) {
-      localObject = Object.assign({}, inputObjectTemplate);
-      break;
+  // for (let key in inputObjectTemplate) {
+  //   if (!localObject[key]) {
+  //     localObject = Object.assign({}, inputObjectTemplate);
+  //     break;
+  //   }
+  // }
+
+  const [localObject, setLocalObject] = useState(
+    JSON.parse(fetchLocalStorage())
+  );
+  useEffect(() => {
+    for (let key in inputObjectTemplate) {
+      if (!localObject[key]) {
+        setLocalObject(inputObjectTemplate);
+        break;
+      }
     }
-  }
+  }, [localObject]);
 
   // const object = { coffeeZ: 'cup', chicken: 'eggsf', number: 33 };
   // localStorage.setItem('Palaver', JSON.stringify(object));
