7bdf436c4cb5546d81665f99ac7d5d5a5a78bf15
Spenser Schwartz <spensernet@gmail.com>
2021-03-11 08:59:51 -0800

    Remove localStorageObject and keep passing down localObject instead

--- src/components/LocalStorageField/LocalStorageDisplay.js
+++ src/components/LocalStorageField/LocalStorageDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 import LocalStorageTextBox from './LocalStorageTextBox';
-import { inputObjectTemplate } from '../../helpers';
+//import { inputObjectTemplate } from '../../helpers';
 
 const LocalStorageDisplay = ({ localObject }) => {
   // const localStorageObject = localObject.companyName
