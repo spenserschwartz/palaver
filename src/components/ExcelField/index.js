f895056410931b51da4aaffc6d21e77a44a412c2
Spenser Schwartz <spensernet@gmail.com>
2021-03-10 16:55:23 -0800

    Update

--- src/components/ExcelField/index.js
+++ src/components/ExcelField/index.js
@@ -8,17 +8,17 @@ const xlsx = require('xlsx');
 const createExcelFile = () => {
   const wb = xlsx.utils.book_new(); // Create a new workbook
   wb.Props = {
-    Title: 'SheetJS Tutorial',
-    Subject: 'This is the subject',
-    Author: 'Red Stapler',
+    Title: 'Palaver Database',
+    Subject: 'Palaver Database',
+    //Author: 'Red Stapler',
     CreatedDate: new Date(),
   };
 
-  wb.SheetNames.push('Test Sheet'); // Assign a new sheet name and push to SheetNames array
+  wb.SheetNames.push('Palaver'); // Assign a new sheet name and push to SheetNames array
   const ws_data = [['hello', 'world']]; // A row with two columns
   const ws = xlsx.utils.aoa_to_sheet(ws_data); // Create the sheet from the ws_data array
-  wb.Sheets['Test Sheet'] = ws; // Assign sheet object to the workbook Sheets array
-  xlsx.writeFile(wb, './newDataFile.xlsx');
+  wb.Sheets['Palaver'] = ws; // Assign sheet object to the workbook Sheets array
+  xlsx.writeFile(wb, 'PalaverDatabase.xlsx');
 };
 
 const ExcelField = () => {
