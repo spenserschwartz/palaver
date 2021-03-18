53d61e0040cc344e4ef75e66d6a4aafc7e24b5c6
Spenser Schwartz <spensernet@gmail.com>
2021-03-18 09:26:51 -0700

    Update

--- src/components/ExcelField/ConvertField/index.js
+++ src/components/ExcelField/ConvertField/index.js
@@ -1,48 +1,10 @@
 import React, { useState } from 'react';
-import { OutTable, ExcelRenderer } from 'react-excel-renderer';
 import XLSX from 'xlsx';
 
 const ConvertField = () => {
-  const [rows, setRows] = useState([]);
-  const [columns, setColumns] = useState([]);
-
-  const onChange = (e) => {
-    console.log('EE: ', e.target.files[0]);
-  };
-
-  const fileHandler = (event) => {
-    let fileObj = event.target.files[0];
-    console.log('This is fileObj: ', fileObj);
-
-    //just pass the fileObj as parameter
-    ExcelRenderer(fileObj, (err, resp) => {
-      if (err) {
-        console.log(err);
-      } else {
-        // this.setState({
-        //   cols: resp.cols,
-        //   rows: resp.rows,
-        // });
-        console.log('hey');
-        setColumns(resp.cols);
-        setRows(resp.rows);
-      }
-    });
-  };
-
   return (
-    <div id="wrapper">
-      <input
-        type="file"
-        onChange={(e) => fileHandler(e)}
-        style={{ padding: '10px' }}
-      />
-      <OutTable
-        data={rows}
-        columns={columns}
-        tableClassName="ExcelTable2007"
-        tableHeaderRowClass="heading"
-      />
+    <div>
+      <h2>ConvertField</h2>
     </div>
   );
 };
