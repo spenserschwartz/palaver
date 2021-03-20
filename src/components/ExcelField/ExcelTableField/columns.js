8d10202a5cc6f4cb0ca6b0944297e6ae544a2ce3
Spenser Schwartz <spensernet@gmail.com>
2021-03-19 17:15:37 -0700

    Install npm react-table and started columns.js

---
+++ src/components/ExcelField/ExcelTableField/columns.js
@@ -0,0 +1,38 @@
+export const COLUMNS = [
+  {
+    Header: 'Status',
+  },
+  {
+    Header: 'Date Submitted',
+  },
+  {
+    Header: 'Company Name',
+  },
+  {
+    Header: 'Job Title',
+  },
+  {
+    Header: 'Location',
+  },
+  {
+    Header: 'Link to Application',
+  },
+  {
+    Header: 'Applied Through',
+  },
+  {
+    Header: 'Internal Contact',
+  },
+  {
+    Header: 'Internal Contact Email',
+  },
+  {
+    Header: 'Double Down Date',
+  },
+  {
+    Header: 'Response Date',
+  },
+  {
+    Header: 'Follow Up Contact(s)',
+  },
+];
