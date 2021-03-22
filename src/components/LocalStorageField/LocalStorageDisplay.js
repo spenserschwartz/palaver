16adf80fd721eb2fdf337f22481ae39a3d5c9d14
Spenser Schwartz <spensernet@gmail.com>
2021-03-22 10:16:00 -0700

    Update

--- src/components/LocalStorageField/LocalStorageDisplay.js
+++ src/components/LocalStorageField/LocalStorageDisplay.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './styles.css';
 import LocalStorageTextBox from './LocalStorageTextBox';
-//import { inputObjectTemplate } from '../../helpers';
 
 const LocalStorageDisplay = ({ localObject }) => {
   return (
