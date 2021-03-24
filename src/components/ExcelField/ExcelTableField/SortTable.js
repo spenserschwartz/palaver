38bac2e399dd1a66b9cb8fdc5cdccde375e56c5f
Spenser Schwartz <spensernet@gmail.com>
2021-03-24 09:52:12 -0700

    SortTable works, basic functionality

---
+++ src/components/ExcelField/ExcelTableField/SortTable.js
@@ -0,0 +1,75 @@
+import React, { useMemo } from 'react';
+import { useTable, useSortBy } from 'react-table';
+import { COLUMNS } from './columns';
+import './table.css';
+
+const SortTable = ({ excelObject }) => {
+  console.log('sortT: ', excelObject['Applications']);
+
+  // useMemo ensures data isn't recreated on every render
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => excelObject['Applications'], [excelObject]) || [];
+  //const data = [];
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
+
+  const firstPageRows = rows.slice(0, 20);
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
+    </>
+  );
+};
+
+export default SortTable;
--- src/components/ExcelField/ExcelTableField/index.js
+++ src/components/ExcelField/ExcelTableField/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BasicTable from './BasicTable';
+import SortTable from './SortTable';
 
 const ExcelTableField = ({ excelObject, setExcelObject }) => {
   console.log('neO: ', excelObject);
   return (
     <div>
       <h3>Excel Table Field</h3>
-      <BasicTable excelObject={excelObject} />
+      {/* <BasicTable excelObject={excelObject} /> */}
+      <SortTable excelObject={excelObject} />
     </div>
   );
 };
