919de66aa37986f4a9b916afe62d51ab06c9ceae
Spenser Schwartz <spensernet@gmail.com>
2021-03-24 18:22:32 -0700

    Moving code from ExcelTable to helpers/excelFunctions

--- src/helpers/excelFunctions.js
+++ src/helpers/excelFunctions.js
@@ -144,9 +144,25 @@ function SelectColumnFilter({
   );
 }
 
+// This is a custom aggregator that
+// takes in an array of leaf values and
+// returns the rounded median
+function roundedMedian(leafValues) {
+  let min = leafValues[0] || 0;
+  let max = leafValues[0] || 0;
+
+  leafValues.forEach((value) => {
+    min = Math.min(min, value);
+    max = Math.max(max, value);
+  });
+
+  return Math.round((min + max) / 2);
+}
+
 export {
   NumberRangeColumnFilter,
   DefaultColumnFilter,
   SliderColumnFilter,
   SelectColumnFilter,
+  roundedMedian,
 };
