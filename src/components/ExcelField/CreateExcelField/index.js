a7048d2b8344fd397fc5128e5d02efdb9a4f6fe2
Spenser Schwartz <spensernet@gmail.com>
2021-03-31 11:02:54 -0700

    Put alert in createExcel for when there is no excelObject

--- src/components/ExcelField/CreateExcelField/index.js
+++ src/components/ExcelField/CreateExcelField/index.js
@@ -2,40 +2,28 @@ import React from 'react';
 const xlsx = require('xlsx');
 
 const CreateExcelField = ({ excelObject }) => {
-  const createExcelFile = () => {
-    const wb = xlsx.utils.book_new(); // Create a new workbook
-    wb.Props = {
-      Title: 'Palaver Database',
-      Subject: 'Palaver Database',
-      //Author: 'Red Stapler',
-      CreatedDate: new Date(),
-    };
-
-    wb.SheetNames.push('Palaver'); // Assign a new sheet name and push to SheetNames array
-    const ws_data = [['hello', 'world']]; // A row with two columns
-    const ws = xlsx.utils.aoa_to_sheet(ws_data); // Create the sheet from the ws_data array
-    wb.Sheets['Palaver'] = ws; // Assign sheet object to the workbook Sheets array
-    xlsx.writeFile(wb, 'createdTestFile.xlsx');
-  };
-
   console.log('CEF eO: ', excelObject);
 
   const createExcelFromExcelObject = () => {
-    const wb = xlsx.utils.book_new(); // Create a new workbook
-    wb.Props = {
-      Title: 'Palaver Database',
-      Subject: 'Palaver Database',
-      //Author: 'Red Stapler',
-      CreatedDate: new Date(),
-    };
+    if (excelObject['Applications']) {
+      const wb = xlsx.utils.book_new(); // Create a new workbook
+      wb.Props = {
+        Title: 'Palaver Database',
+        Subject: 'Palaver Database',
+        //Author: 'Red Stapler',
+        CreatedDate: new Date(),
+      };
 
-    wb.SheetNames.push('Applications'); // Assign a new sheet name and push to SheetNames array
-    const applicationsWS = xlsx.utils.json_to_sheet(
-      excelObject['Applications']
-    );
+      wb.SheetNames.push('Applications'); // Assign a new sheet name and push to SheetNames array
+      const applicationsWS = xlsx.utils.json_to_sheet(
+        excelObject['Applications']
+      );
 
-    wb.Sheets['Applications'] = applicationsWS;
-    xlsx.writeFile(wb, 'testyEO.xlsx');
+      wb.Sheets['Applications'] = applicationsWS;
+      xlsx.writeFile(wb, 'testyEO.xlsx');
+    } else {
+      window.alert('You need something in excelObject');
+    }
   };
 
   return (
@@ -46,9 +34,6 @@ const CreateExcelField = ({ excelObject }) => {
         onClick={() => createExcelFromExcelObject()}
       >
         Create from excelObject
-      </button>
-      <button className="button green" onClick={() => createExcelFile()}>
-        Create Excel File
       </button>
     </div>
   );
