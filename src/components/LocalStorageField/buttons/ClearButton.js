e469a0ac8deb89e75c9a4dffbd6b11e16da77b7f
spenserschwartz <spensernet@gmail.com>
2021-04-06 22:59:21 -0700

    Clear Local Storage Button now clears values in text boxes to reflect local storage clear

--- src/components/LocalStorageField/buttons/ClearButton.js
+++ src/components/LocalStorageField/buttons/ClearButton.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { inputObjectTemplate } from '../../../helpers';
+import React from "react";
+import {
+  inputObjectTemplate,
+  localStorageObjectTemplate,
+} from "../../../helpers";
 
 const ClearButton = () => {
   const clearLocalStorage = () => {
-    if (window.confirm('Are you sure you want to clear Local Storage?')) {
-      if (window.confirm('Are you absolutely sure?')) {
-        localStorage.setItem('Palaver', JSON.stringify(inputObjectTemplate));
+    if (window.confirm("Are you sure you want to clear Local Storage?")) {
+      if (window.confirm("Are you absolutely sure?")) {
+        localStorage.setItem("Palaver", JSON.stringify(inputObjectTemplate));
+
+        // Clear current textbox values
+        for (let key in localStorageObjectTemplate) {
+          let inputTextID = "localStorageTextBox" + key;
+          document.getElementById(inputTextID).value = "";
+        }
       }
     }
-    // localStorage.setItem('Palaver', JSON.stringify(inputObjectTemplate));
   };
 
   return (
